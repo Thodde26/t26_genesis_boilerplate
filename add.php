8826cc661e4e2e3c541a0f941cbeed7488641a13
Thodde26 <Thodde26@hotmail.com>
2026-06-21 07:38:01 +0200

    Add files via upload

---
+++ add.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * T26 Genesis Boilerplate - WBCE CMS
+ * @author      Thodde26 (Thorsten)
+ * @link        https://www.thodde26.de
+ * @file        add.php
+ * @license     http://www.gnu.org/licenses/gpl.html
+ * GNU General Public License v3.0
+ */
+
+if (!defined('WB_PATH')) {
+  die('Cannot access this file directly');
+}
---
+++ save.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * T26 Genesis Core - WBCE CMS
+ * @author      Thodde26 (Thorsten)
+ * @link        https://www.thodde26.de
+ * @Version     1.0.0
+ * @file        save.php
+ * @license     http://www.gnu.org/licenses/gpl.html
+ * GNU General Public License v3.0
+ */
+
+if (!defined('WB_PATH')) {
+  die('Cannot access this file directly');
+}
